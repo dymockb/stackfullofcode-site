47058b5b2b1edf55a5e262c0196fcf6cd0388bf0
db <22858501+dymockb@users.noreply.github.com>
2021-09-22 14:38:50 +0100

    tidy JS files and folder

--- project1/libs/php/oneRestCountry.php
+++ project1/libs/php/oneRestCountry.php
@@ -24,6 +24,7 @@
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
 	$output['data'] = $decode;
+	$output['cCode'] = $_REQUEST['countryCode'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
