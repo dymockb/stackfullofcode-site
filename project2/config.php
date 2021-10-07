114de0d3e072a6b9487e9574f51279394c5fc44c
db <22858501+dymockb@users.noreply.github.com>
2021-10-07 18:16:29 +0100

    add config for tsohost

---
+++ project2/config.php
@@ -0,0 +1,16 @@
+
+<?php
+
+	// connection details for MySQL database
+
+	$cd_host = "localhost";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+
+	$cd_dbname = "companydirectory";
+	$cd_user = "project2user";
+	$cd_password = "p2user";
+
+?>
